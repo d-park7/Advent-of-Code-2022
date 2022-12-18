4c485e804d3bf8f7949713c350b78d335a226f3b
du.mark-heb <du.mark@heb.com>
2022-12-17 18:49:50 -0600

    Add input files for day 11

--- day10/main.py
+++ day10/main.py
@@ -1,3 +1,6 @@
+# Build a queue of dictionaries ({addx_value : turnCost })
+# for each cycle decrease front turnCost, if 0 then remove and add value to counter
+
 with open('input_test.txt', 'r') as file:
 
     cycle = 1
---
+++ day11/main.py
@@ -0,0 +1,4 @@
+# Create a dictionary of 
+# {monkeyIndex : starting items}
+# {monkeyIndex : operationFunction}
+# Operation function will do the operation, test, and then perform throw
