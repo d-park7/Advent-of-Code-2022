3f23866c2e3085fabfa95ec19c68d8dec43ac269
jems-lee <jameslee.j7@gmail.com>
2022-12-20 15:19:04 -0500

    day02 solutions for part 1 and 2

--- day02/solution.lua
+++ day02/solution.lua
@@ -29,10 +29,38 @@ local symbol_map = {
 
 local total = 0
 for round in io.lines() do
-  print(round)
   local opp = symbol_map[string.sub(round, 1, 1)]
   local you = symbol_map[string.sub(round, 3, 3)]
   total = total + score_round(opp, you)
 end
 
 print(total)
+
+-- part 2
+-- X = lose
+-- Y = draw
+-- Z = win
+
+local pick_correct_shape = function(left, right)
+  if right == "X" then -- lose
+    return (symbol_map[left] - 2) % 3 + 1
+  elseif right == "Y" then -- draw
+    return symbol_map[left]
+  else -- win
+    return symbol_map[left] % 3 + 1
+  end
+end
+
+local total2 = 0
+
+io.input("./input.txt")
+for round in io.lines() do
+  print(round)
+  local opp_letter = string.sub(round, 1, 1)
+  local you_letter = string.sub(round, 3, 3)
+  local opp = symbol_map[opp_letter]
+  local your_pick = pick_correct_shape(opp_letter, you_letter)
+  total2 = total2 + score_round(opp, your_pick)
+end
+
+print(total2)
