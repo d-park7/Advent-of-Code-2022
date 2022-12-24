9729dbae64387a6991103efe7e7124529705d3b0
Mark <2017mark@gmail.com>
2022-12-24 17:34:23 -0600

    Day 12 part 1 work

--- day12/main.py
+++ day12/main.py
@@ -1,3 +1,21 @@
+from heapq import heappop, heappush
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+  row: int
+  col: int
+  visited: bool
+  
+  def __init__(self, x, y, visited=False) -> None:
+    self.row = x
+    self.col = y
+    self.visited = visited
+    
+  def __eq__(self, other) -> bool:
+    return self.row == other.row and self.col == other.col
+    
+
 grid = []
 with open('input_test.txt', 'r') as file:
   grid = [list(row) for row in file.read().splitlines()]
@@ -16,17 +34,28 @@ def height(row, col):
 for row in range(len(grid)):
   for col in range(len(grid[row])):
     if grid[row][col] == 'S':
-      start = (row, col)
+      start = Node(row, col)
     elif grid[row][col] == 'E':
-      end = (row, col)
-      
-# Set node to starting point, and then run algorithm
-def travel():
-  visited = []
-  prio_q = []
-  node = start
+      end = Node(row, col)
+
+# Start and end are nodes containing (x, y, visited) position on grid
+def travel(start, end):
+  visited = set()
+  pq = [] 
+  heappush(pq, start)   # Push start node onto queue  
+  
+  while len(pq) > 0:
+    curr_steps, node = heappop(pq)
+    if not node.visited:
+      node.visited = True
+      if node == end:
+        return curr_steps
+      else:
+        pass  # Logic for adding new neighbor nodes here
+
   
   
+travel(start, end) 
   
   
   
